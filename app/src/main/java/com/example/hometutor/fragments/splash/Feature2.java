45034b8dc7c1119408fbfe064d5236240153b6d3
mahadi10040 <mahadi10040@gmail.com>
2021-05-02 16:48:57 +0600

    only one fragment left splash

--- app/src/main/java/com/example/hometutor/fragments/splash/Feature2.java
+++ app/src/main/java/com/example/hometutor/fragments/splash/Feature2.java
@@ -7,17 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.example.hometutor.R;
+import com.example.hometutor.classes.ChangeFragment;
 
-public class Feature2 extends Fragment {
+public class Feature2 extends Fragment implements ChangeFragment.FragmentChange {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view=  inflater.inflate(R.layout.fragment_feature2, container, false);
+        View view =  inflater.inflate(R.layout.fragment_feature2, container, false);
+
+        ImageView next = view.findViewById(R.id.feature2_next);
+        next.setOnClickListener(v->changingFragment(new ChangeFragment()));
 
         return view;
+    }
+
+    @Override
+    public void changingFragment(ChangeFragment change) {
+        change.OneFragmentToAnother(R.id.splash_layout, this, new Feature3());
     }
 }
--- app/src/main/java/com/example/hometutor/fragments/splash/Feature3.java
+++ app/src/main/java/com/example/hometutor/fragments/splash/Feature3.java
@@ -1,5 +1,6 @@
 package com.example.hometutor.fragments.splash;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,8 +8,12 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import com.example.hometutor.AuthenticationActivity;
 import com.example.hometutor.R;
+
+import java.util.Objects;
 
 public class Feature3 extends Fragment {
 
@@ -18,6 +23,14 @@ public class Feature3 extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_feature3, container, false);
 
+        Button begin = view.findViewById(R.id.feature_begin);
+        begin.setOnClickListener(v->goToAuthenticationActivity());
+
         return view;
+    }
+
+    private void goToAuthenticationActivity() {
+        startActivity(new Intent(getActivity(), AuthenticationActivity.class));
+        Objects.requireNonNull(getActivity()).finish();
     }
 }
