026be3d8736c600f59efbeb4562305ae1d30dcfb
mahadi10040 <mahadi10040@gmail.com>
2021-05-02 15:24:45 +0600

    welcome done

--- app/src/main/java/com/example/hometutor/SplashActivity.java
+++ app/src/main/java/com/example/hometutor/SplashActivity.java
@@ -3,6 +3,9 @@ package com.example.hometutor;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.hometutor.fragments.splash.Welcome;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -10,5 +13,11 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        if (savedInstanceState == null){
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.splash_layout, new Welcome()).commit();
+        }
     }
 }
--- app/src/main/java/com/example/hometutor/fragments/splash/Welcome.java
+++ app/src/main/java/com/example/hometutor/fragments/splash/Welcome.java
@@ -3,21 +3,37 @@ package com.example.hometutor.fragments.splash;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.example.hometutor.R;
+
+import java.util.Objects;
 
 public class Welcome extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+
         View view = inflater.inflate(R.layout.fragment_welcome, container, false);
 
+        Button getStarted = view.findViewById(R.id.welcome_getStarted);
+        getStarted.setOnClickListener(v -> goToFeature1());
+
         return view;
+    }
+
+    private void goToFeature1() {
+        FragmentManager manager = Objects.requireNonNull(getActivity()).getSupportFragmentManager();
+        FragmentTransaction transaction = manager.beginTransaction();
+        transaction.replace(R.id.splash_layout, new Feature1());
+        transaction.addToBackStack(null);
+        transaction.commit();
     }
 }
