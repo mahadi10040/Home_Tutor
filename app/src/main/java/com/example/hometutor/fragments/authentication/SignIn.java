d4acc6f29fb52c1a3c753c75e2cc4a4a5c286f37
mahadi10040 <mahadi10040@gmail.com>
2021-05-02 21:12:17 +0600

    auth design ends

--- app/src/main/java/com/example/hometutor/fragments/authentication/SignIn.java
+++ app/src/main/java/com/example/hometutor/fragments/authentication/SignIn.java
@@ -7,15 +7,27 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.hometutor.R;
+import com.example.hometutor.classes.ChangeFragment;
 
-public class SignIn extends Fragment {
+public class SignIn extends Fragment implements ChangeFragment.FragmentChange {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_sign_in, container, false);
+
+        TextView newUser = view.findViewById(R.id.signIn_newUser);
+        newUser.setOnClickListener(v -> changingFragment(new ChangeFragment()));
+
+        return view;
+    }
+
+    @Override
+    public void changingFragment(ChangeFragment change) {
+        change.OneFragmentToAnother(R.id.authentication_layout,this, new SignUp());
     }
 }
